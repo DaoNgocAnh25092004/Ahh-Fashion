6e1e73e6a1607d29fd7a35b84e5116a6e6b53ab9
pharmacy <thuyhienle1664@gmail.com>
2025-02-28 14:54:03 +0700

    new code header

--- src/Layouts/Components/Header/Header.js
+++ src/Layouts/Components/Header/Header.js
@@ -15,9 +15,12 @@ import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import Button from '~/components/Button';
 const cx = classNames.bind(styles);
 
 function Header() {
+
+
     return (
         <header className={cx('header')}>
             <div>
@@ -31,27 +34,38 @@ function Header() {
                     <Menu>
                         <MenuItem title="HOME" to={config.routes.home} icon={<FontAwesomeIcon icon={faHome} />} />
                         <MenuItem title="ALL FRODUCT" to={config.routes.home} icon={null} />
-                        <MenuItem title="TOPS" to={config.routes.home} icon={null} />
-                        <MenuItem title="BOTTOMS" to={config.routes.home} icon={null} />
+                        <MenuItem className={cx("listShirt")} title="TOPS" to={config.routes.home} icon={null} >
+                            <MenuItem title="SHIRT" to={config.routes.shirt} />
+                            <MenuItem title="T-SHIRT" to={config.routes.tshirt} />
+                            <MenuItem title="STYLE DRESS" to={config.routes.style} />
+                        </MenuItem>
+                        <MenuItem className={cx("listShort")} title="BOTTOMS" to={config.routes.home} icon={null} >
+                            <MenuItem title="SHORTS" to={config.routes.shirt} />
+                            <MenuItem title="TROUSER" to={config.routes.tshirt} />
+                        </MenuItem>
                         <MenuItem title="OUTERWEAR" to={config.routes.home} icon={null} />
                         <MenuItem title="ACCESSORIES" to={config.routes.home} icon={null} />
-                        <MenuItem title="SALE" to={config.routes.home} icon={null} />
-
+                        <MenuItem className={cx("listDiscount")} title="SALE" to={config.routes.home} icon={null} >
+                            <MenuItem title="Discount 10%" to={config.routes.discount10} />
+                            <MenuItem title="Discount 20%" to={config.routes.discount20} />
+                            <MenuItem title="Clearance Sale" to={config.routes.clearanceSale} />
+                        </MenuItem>
                     </Menu>
                     <div className={cx("list--icon")}>
                         <FontAwesomeIcon icon={faHeart} className={cx("icon", "heart-icon")} />
-                        <FontAwesomeIcon icon={faUser} className={cx("icon", "user-icon")} />
+                        <div className={cx("user-container")}>
+                            <FontAwesomeIcon icon={faUser} className={cx("icon", "user-icon")} />
+                            <div className={cx("list-btn")}>
+                                <span className={cx("login-text")}>Đăng nhập</span>
+                                <span className={cx("register-text")}>Đăng ký</span>
+                            </div>
+                        </div>
                         <FontAwesomeIcon icon={faBasketShopping} className={cx("icon", "basket-icon")} />
                     </div>
                 </div>
             </div>
-
-
-
         </header>
-
     );
-
 }
 
 Header.propTypes = {
--- src/Layouts/Components/Header/Menu/MenuItem.js
+++ src/Layouts/Components/Header/Menu/MenuItem.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
@@ -5,12 +6,19 @@ import styles from './Menu.module.scss';
 
 const cx = classNames.bind(styles);
 
-function MenuItem({ title, to, icon }) {
+function MenuItem({ title, to, icon, className, children }) {
+    const [isDropdownOpen, setDropdownOpen] = useState(false);
+
     return (
-        <NavLink className={cx('menu-item')} to={to} >
-            {icon}
-            <span className={cx('title')}>{title}</span>
-        </NavLink>
+        <div className={cx("menu-item", className)}
+            onMouseEnter={() => setDropdownOpen(true)}
+            onMouseLeave={() => setDropdownOpen(false)}>
+            <NavLink className={cx("menu-link")} to={to}>
+                {icon}
+                <span className={cx("title")}>{title}</span>
+            </NavLink>
+            {children && isDropdownOpen && <div className={cx("dropdown")}>{children}</div>}
+        </div>
     );
 
 }
@@ -19,6 +27,8 @@ MenuItem.propTypes = {
     title: PropTypes.string.isRequired,
     to: PropTypes.string.isRequired,
     icon: PropTypes.node.isRequired,
+    className: PropTypes.string,
+    children: PropTypes.node,
 }
 export default MenuItem;
 
